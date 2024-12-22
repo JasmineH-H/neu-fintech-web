b7098299492547192e3583e33d509abb4345153d
Nova Shang <17600549973@163.com>
2024-12-21 21:17:30 -0800

    add responsive navbar with toggle functionality
    12.21

--- js/script.js
+++ js/script.js
@@ -21,3 +21,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+const mobileMenu = () => {
+  menu.classList.toggle('is-active')
+  menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu)
